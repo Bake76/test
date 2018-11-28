f4d76577516276b94ddfa46e9fd6474dfe47a406
Thorsten Becker <mail@thorstenbecker.net>
2018-11-28 02:01:57 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,30 @@
+
+// var nomen;
+// var verb;
+// var adjektiv;
+
+// function abfrage(wort) {
+//    var ergebnis = prompt("Gib ein " + wort + " ein");
+//    return ergebnis;
+// }
+
+// for (i = 3; i > 0; i--) {
+//    switch (i) {
+//       case 3: 
+//          nomen = abfrage("Nomen");
+//          break;
+//       case 2:
+//          verb = abfrage("Verb");
+//          break;
+//       default:
+//          adjektiv = abfrage("Adjektiv");
+//    }
+// }
+
+// message = "Nomen: " + nomen + "<br>";
+// message += "Verb: " + verb + "<br>";
+// message += "Adjektiv: " + adjektiv;
+
+// document.write(message);
+
+
